239340dfce64239fee609d3c82cd9253c8387508
saquib__ali <76240103+saquibali7@users.noreply.github.com>
2022-03-14 22:38:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (27.54%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int temp = x^y;
+        int res = 0;
+        while(temp>0){
+            temp = (temp &(temp-1));
+            res++;
+        }
+        return res;
+        
+    }
+};
