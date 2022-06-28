7507091b72cbf715913b48bed5774f0c1a0df7f4
saquib__ali <76240103+saquibali7@users.noreply.github.com>
2022-06-28 12:10:29 +0530

    Time: 9 ms (6.07%), Space: 5.9 MB (68.76%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        unordered_set<int> s;
-        if(n<0 || n==0){
+        if(n<=0){
             return false;
         }
-        while(n%2==0){
-            s.insert(2);
-            n/=2;
-        }
-        for(int i=3;i<=sqrt(n);i=i+2){
-            while(n%i==0){
-                s.insert(i);
-                n/=i;
+        while(n != 1){
+            if(n%2 == 0){
+                n/=2;
+            } else if(n%3 == 0){
+                n/=3;
+            } else if(n%5 == 0){
+                n/=5;
+            }
+            else {
+                return false;
             }
         }
-        if(n>2){
-            s.insert(n);
-        }
-        for(auto it : s){
-            if(it==2 || it==3 || it==5){
-                continue;
-            }
-            return false;
-        }
-        return true;
-        
+         return true;
     }
 };
