56c84c781ef2d7253a216409b7ec2a04e6f0fbc0
saquib__ali <76240103+saquibali7@users.noreply.github.com>
2022-07-28 22:27:40 +0530

    Time: 15 ms (43.59%), Space: 13.2 MB (5.50%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int, string>> temp;
+        temp.push_back({1000, "M"});
+        temp.push_back({900, "CM"});
+        temp.push_back({500, "D"});
+        temp.push_back({400, "CD"});
+        temp.push_back({100, "C"});
+        temp.push_back({90, "XC"});
+        temp.push_back({50, "L"});
+        temp.push_back({40, "XL"});
+        temp.push_back({10, "X"});
+        temp.push_back({9, "IX"});
+        temp.push_back({5, "V"});
+        temp.push_back({4, "IV"});
+        temp.push_back({1, "I"});
+        string res="";
+        while(num > 0){
+            for(int i=0;i<temp.size();i++){
+                int s= num/temp[i].first;
+                for(int j=0;j<s;j++){
+                    res+=temp[i].second;
+                }
+                num%=temp[i].first;
+            }
+        }
+        return res;
+        
+    }
+};
